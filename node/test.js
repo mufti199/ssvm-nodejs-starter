c248fe7c79ce68e040c5b60e592d938ba205c1f9
Gazi Mufti Mahmood <mufti199@pop-os.localdomain>
2020-08-27 00:45:05 +1000

    new sentence added

--- node/test.js
+++ node/test.js
@@ -1,2 +1,2 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
-console.log( say("Node.js") );
+const { say } = require("../pkg/ssvm_nodejs_starter_lib.js.js.js");
+console.log(say("Node.js"));
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,6 @@ use wasm_bindgen::prelude::*;
 pub fn say(s: &str) -> String {
   println!("The Rust function say() received {}", s);
   let r = String::from("hello ");
+  let t = "\nI hope you are having a great day!"
   return r + s;
 }
